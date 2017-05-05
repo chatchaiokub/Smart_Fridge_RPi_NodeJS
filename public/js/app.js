7b57bcb4c58bfe8e8e6b8fc57b13ce7b3adb4da8
Chatchaiokub <chatchaiokub@Chatchaiokubs-MacBook-Pro.local>
2017-05-06 06:04:05 +0700

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -4,51 +4,29 @@ angular.module('dragApp', [])
   $scope.drag = []
   $scope.freezer = []
   $scope.index = ''
-  var x = 0
+
   $scope.getData = function () {
     console.log('get ครั้งที่ 1')
     $http.get('/api').success(function (response) {
       $scope.drag = response
       $scope.cal()
       // ////// LED Check //////
-      // $scope.CheckData = []
-      // for (var i = 0; i < $scope.drag.length; i++) {
-      //   $scope.CheckData[i] = $scope.drag[i].endDate
-      //   var now = new Date()
-      //   var datePick = new Date($scope.CheckData[i])
-      //   var SUMDATA = Math.ceil((datePick - now) / (1000 * 3600 * 24))
-      //   if (SUMDATA <= 0) {
-      //     console.log('เข้าในนี้ไหม 1')
-      //     $scope.state = 1
-      //     x = 1
-      //     console.log($scope.state, 'state')
-      //   }if (SUMDATA > 0) {
-      //     $scope.stood = 1
-      //     console.log($scope.stood, 'stood')
-      //   }
-      // }
-      // console.log($scope.state, $scope.stood, 'gggggggg')
+      $scope.CheckData = []
+      for (var i = 0; i < $scope.drag.length; i++) {
+        $scope.CheckData[i] = $scope.drag[i].endDate
+        var now = new Date()
+        var datePick = new Date($scope.CheckData[i])
+        var SUMDATA = Math.ceil((datePick - now) / (1000 * 3600 * 24))
+        if (SUMDATA <= 0) {
+          $scope.state = 1
+        }if (SUMDATA > 0) {
+          $scope.stood = 1
+        }
+      }
       // ////// LED Check //////
     })
-    // console.log($scope.state, $scope.stood, 'getapi and return')
   }
-  console.log(x, 'xxx')
   $scope.getData()
-  $scope.cal = function () {
-    $scope.CheckData = []
-    console.log($scope.drag, 'aaa')
-    for (var i = 0; i < $scope.drag.length; i++) {
-      $scope.CheckData[i] = $scope.drag[i].endDate
-      var now = new Date()
-      var datePick = new Date($scope.CheckData[i])
-      var SUMDATA = Math.ceil((datePick - now) / (1000 * 3600 * 24))
-      if (SUMDATA <= 0) {
-        x = 1
-      }if (SUMDATA > 0) {
-      }
-    }
-  }
-  console.log(x, 'yyy')
   $scope.positionDrag = function (index) {
     var css = $('#' + index).position()
     css.position = 'absolute'
@@ -174,12 +152,9 @@ angular.module('dragApp', [])
         var datePick = new Date($scope.CheckFreezer[i])
         var SUMDATA = Math.ceil((datePick - now) / (1000 * 3600 * 24))
         if (SUMDATA <= 0) {
-          console.log('เข้าในนี้ไหม 2')
           $scope.state = 1
-          console.log($scope.state, 'state freezer')
         }if (SUMDATA > 0) {
           $scope.stood = 1
-          console.log($scope.stood, 'stood freezer')
         }
       }
       // ////// LED Check //////
