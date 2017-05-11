bd81d95758996e2de5ae13ba04c787abf2c7658b
chatchaiokub <chatty30433@windowslive.com>
2017-05-11 03:34:59 +0000

    fix

--- drink2.py
+++ drink2.py
@@ -1,93 +1,77 @@
-#!/usr/bin/python
-import requests
-import json
 import spidev
-import time
+import time 
 
-ldr_channel = 0
+analog_ch = 1
 
-#Create SPI
-spi = spidev.SpiDev()
-spi.open(0, 0)
+spi =spidev.SpiDev()
+spi.open(0,0)
 
-def analogRead(data):
+def readADC(adcnum):
+	if adcnum > 7 or adcnum < 0:
+		return -1
+	r =spi.xfer2([4 | 2 | (adcnum >> 2), (adcnum & 3) << 6, 0])
+	adcout = ((r[1] & 15) << 8) + r[2]
+	return adcout
 
-    	if data > 7 or data < 0:
-        	return -1
-
-    	r = spi.xfer2([1, 8 + data << 4, 0])
-    	data = ((r[1] & 3) << 8) + r[2]
-
-    	return data
-
-def sendDataEGG(data):
-	print ("Sending..... ",data)
-	url = 'http://localhost:3000/dataEgg'
-       	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-	payload = {'egg': data}
+def sendDataDRINK(data):
+        print ("Sending..... ",data)
+        url = 'http://localhost:3000/dataDrink'
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        payload = {'drink': data}
         response = requests.post(url,data=payload)
 
-#def sendDataDRINK(data):
-#        print ("Sending..... ",data)
-#        url = 'http://localhost:3000/dataDrink'
-#        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-#        payload = {'drink': data}
-#        response = requests.post(url,data=payload)
-
-check = -1
-#CHECK = -1
+CHECK = -1
 
 while True:
+	value = readADC(analog_ch)
+	print("analog_ch1=",value)
+	time.sleep(0.3)
+	
+	if val >= 1006:
+                print ("No",val)
+                if (check == 1):
+                        check = 0
+                        sendDataEGG(0)
+                elif (check == 0):
+                        check = 0
+                elif (check == -1):
+                        check = 0
+                        sendDataEGG(0)
 
-    	val = analogRead(ldr_channel)
-	time.sleep(0.5)
-
-
-    	if val >= 1006:
-		print ("No",val)
-		if (check == 1):
-			check = 0
-			sendDataEGG(0)
-		elif (check == 0):
-			check = 0
-		elif (check == -1):
-			check = 0
-			sendDataEGG(0)
-
-    	elif val < 1005 and val >= 980:
+        elif val < 1005 and val >= 980:
 		print ("1 egg",val)
-		if (check == 0):
-			check = 1
-			sendDataEGG(1)
-		elif (check == 1):
-			check = 1
-		else:
+                if (check == 0):
+                        check = 1
+                        sendDataEGG(1)
+                elif (check == 1):
+                        check = 1
+                else:
                         check = 1
                         sendDataEGG(1)
 
-    	elif val <= 979 and val >= 955:
-        	print ("2 egg",val)
-		if (check == 1):
+	elif val <= 979 and val >= 955:
+                print ("2 egg",val)
+                if (check == 1):
                         check = 2
                         sendDataEGG(2)
                 elif (check == 2):
                         check = 2
-		else:
-			check = 2
-			sendDataEGG(2)
-
+                else:
+                        check = 2
+                        sendDataEGG(2)
+	
 	elif val <= 954 and val >= 935:
-               print ("3 egg",val)
-               if (check == 2):
+               	print ("3 egg",val)
+               	if (check == 2):
                         check = 3
                         sendDataEGG(3)
-               elif (check == 3):
+               	elif (check == 3):
                         check = 3
-	       else:
-			check = 3
-			sendDataEGG(3)
+               	else:
+                        check = 3
+                        sendDataEGG(3)
 
-	elif val <= 979 and val >= 955:
+        elif val <= 979 and val >= 955:
                 print ("4 egg",val)
 
                 if (check == 3):
@@ -98,10 +82,7 @@ while True:
                 else:
                         check = 4
                         sendDataEGG(4)
-			url = 'http://localhost:3000/setupEgg'
-			headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-			response = requests.get(url,headers=headers)
-
+             
 	elif val <= 979 and val >= 955:
                 print ("5 egg",val)
 
@@ -126,7 +107,7 @@ while True:
                         check = 6
                         sendDataEGG(6)
 
-	elif val <= 979 and val >= 955:
+        elif val <= 979 and val >= 955:
                 print ("7 egg",val)
 
                 if (check == 6):
@@ -162,7 +143,7 @@ while True:
                         check = 9
                         sendDataEGG(9)
 
-	elif val <= 979 and val >= 955:
+        elif val <= 979 and val >= 955:
                 print ("10 egg",val)
 
                 if (check == 9):
@@ -170,3 +151,28 @@ while True:
                         sendDataEGG(10)
                 elif (check == 10):
                         check = 10
+		else:
+                        check = 10
+                        sendDataEGG(10)
+	
+	elif val <= 979 and val >= 955:
+                print ("11 ",val)
+
+                if (check == 10):
+                        check = 11
+                        sendDataDRINK(11)
+                elif (check == 11):
+                        check = 11
+                else:
+                        check = 11
+                        sendDataDRINK(11)
+
+	elif val <= 979 and val >= 955:
+                print ("12 ",val)
+
+                if (check == 11):
+                        check = 12
+                        sendDataDRINK(12)
+                elif (check == 12):
+                        check = 12
+             
