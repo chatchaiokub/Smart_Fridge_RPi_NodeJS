d23e2fe0782827328c4756851e8b0be355f5a643
chatchaiokub <chatty30433@windowslive.com>
2017-05-19 18:09:14 +0000

    tray

--- drink.py
+++ drink.py
@@ -4,7 +4,7 @@ import json
 import spidev
 import time
 
-channel = 0
+channel = 1
 
 #Create SPI
 spi = spidev.SpiDev()
@@ -23,7 +23,7 @@ def analogRead(data):
 
     	return data
 
-def sendDataEGG(data):
+def sendDataDRINK(data):
 
 	global temp
 	global count
@@ -35,9 +35,9 @@ def sendDataEGG(data):
 		count = count+1
 	if(count == 3):
 		print ("Sending..... ",data)
-		url = 'http://localhost:3000/dataEgg'
+		url = 'http://localhost:3000/dataDrink'
        		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-		payload = {'egg': data}
+		payload = {'drink': data}
         	response = requests.post(url,data=payload)
 	
 check = -1
@@ -61,8 +61,8 @@ while True:
 		#elif (check == -1):
 		#	check = 0
 		#	sendDataEGG(0)
-		sendDataEGG(0)
-    	elif val <= 1017 and val >= 1013:
+		sendDataDRINK(0)
+    	elif val <= 1013 and val >= 1005:
 		print ("1 egg",val)
 		#if (check == 0):
 		#	check = 1
@@ -72,8 +72,8 @@ while True:
 		#else:
                 #        check = 1
                 #        sendDataEGG(1)
-		sendDataEGG(1)
-    	elif val <= 1008 and val >= 1003:
+		sendDataDRINK(1)
+    	elif val <= 1004 and val >= 992:
         	print ("2 egg",val)
 		#if (check == 1):
                 #        check = 2
@@ -83,8 +83,8 @@ while True:
 		#else:
 		#	check = 2
 		#	sendDataEGG(2)
-		sendDataEGG(2)
-	elif val <= 1002 and val >= 997:
+		sendDataDRINK(2)
+	elif val <= 991 and val >= 984:
                print ("3 egg",val)
                #if (check == 2):
                #         check = 3
@@ -94,8 +94,8 @@ while True:
 	       #else:
 	       #         check = 3
 	       #	 sendDataEGG(3)
-	       sendDataEGG(3)
-	elif val <= 995 and val >= 989:
+	       sendDataDRINK(3)
+	elif val <= 983 and val >= 976:
                 print ("4 egg",val)
 
                 #if (check == 3):
@@ -109,8 +109,8 @@ while True:
 		#	url = 'http://localhost:3000/setupEgg'
 		#	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 		#	response = requests.get(url,headers=headers)
-		sendDataEGG(4)
-	elif val <= 987 and val >= 981:
+		sendDataDRINK(4)
+	elif val <= 975 and val >= 966:
                 print ("5 egg",val)
 
                 #if (check == 4):
@@ -121,8 +121,8 @@ while True:
                 #else:
                  #       check = 5
                   #      sendDataEGG(5)
-		sendDataEGG(5)
-	elif val <= 980 and val >= 975:
+		sendDataDRINK(5)
+	elif val <= 965 and val >= 954:
                 print ("6 egg",val)
 
                 #if (check == 5):
@@ -133,8 +133,8 @@ while True:
                 #else:
                  #       check = 6
                   #      sendDataEGG(6)
-		sendDataEGG(6)
-	elif val <= 974 and val >= 968:
+		sendDataDRINK(6)
+	elif val <= 953 and val >= 948:
                 print ("7 egg",val)
 
                 #if (check == 6):
@@ -145,8 +145,8 @@ while True:
                 #else:
                  #       check = 7
                   #      sendDataEGG(7)
-		sendDataEGG(7)
-	elif val <= 967 and val >= 962:
+		sendDataDRINK(7)
+	elif val <= 947 and val >= 940:
                 print ("8 egg",val)
 
                 #if (check == 7):
@@ -157,9 +157,9 @@ while True:
                 #else:
                  #       check = 8
                   #      sendDataEGG(8)
-		sendDataEGG(8)
-	elif val <= 961 and val >= 956:
-                print ("9 egg",val)
+		sendDataDRINK(8)
+	elif val <= 939 and val >= 930:
+                print ("9 drink",val)
 
                 #if (check == 8):
                  #       check = 9
@@ -169,13 +169,4 @@ while True:
                 #else:
                  #       check = 9
                   #      sendDataEGG(9)
-		sendDataEGG(9)
-	elif val <= 955 and val >= 900:
-                print ("10 egg",val)
-
-                #if (check == 9):
-                 #       check = 10
-                  #      sendDataEGG(10)
-                #elif (check == 10):
-                 #       check = 10
-		sendDataEGG(10)
+		sendDataDRINK(9)
--- newegg.py
+++ newegg.py
@@ -49,7 +49,6 @@ while True:
     	val = analogRead(channel)
 	time.sleep(0.5)
 
-	print val
 
     	if val > 1020:
 		print ("No",val)
@@ -62,7 +61,7 @@ while True:
 		#	check = 0
 		#	sendDataEGG(0)
 		sendDataEGG(0)
-    	elif val <= 1017 and val >= 1013:
+    	elif val <= 1019 and val >= 1001:
 		print ("1 egg",val)
 		#if (check == 0):
 		#	check = 1
@@ -73,7 +72,7 @@ while True:
                 #        check = 1
                 #        sendDataEGG(1)
 		sendDataEGG(1)
-    	elif val <= 1008 and val >= 1003:
+    	elif val <= 1000 and val >= 983:
         	print ("2 egg",val)
 		#if (check == 1):
                 #        check = 2
@@ -84,7 +83,7 @@ while True:
 		#	check = 2
 		#	sendDataEGG(2)
 		sendDataEGG(2)
-	elif val <= 1002 and val >= 997:
+	elif val <= 982 and val >= 968:
                print ("3 egg",val)
                #if (check == 2):
                #         check = 3
@@ -95,7 +94,7 @@ while True:
 	       #         check = 3
 	       #	 sendDataEGG(3)
 	       sendDataEGG(3)
-	elif val <= 995 and val >= 989:
+	elif val <= 967 and val >= 950:
                 print ("4 egg",val)
 
                 #if (check == 3):
@@ -110,7 +109,7 @@ while True:
 		#	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 		#	response = requests.get(url,headers=headers)
 		sendDataEGG(4)
-	elif val <= 987 and val >= 981:
+	elif val <= 949 and val >= 939:
                 print ("5 egg",val)
 
                 #if (check == 4):
@@ -122,7 +121,7 @@ while True:
                  #       check = 5
                   #      sendDataEGG(5)
 		sendDataEGG(5)
-	elif val <= 980 and val >= 975:
+	elif val <= 938 and val >= 928:
                 print ("6 egg",val)
 
                 #if (check == 5):
@@ -134,7 +133,7 @@ while True:
                  #       check = 6
                   #      sendDataEGG(6)
 		sendDataEGG(6)
-	elif val <= 974 and val >= 968:
+	elif val <= 927 and val >= 912:
                 print ("7 egg",val)
 
                 #if (check == 6):
@@ -146,7 +145,7 @@ while True:
                  #       check = 7
                   #      sendDataEGG(7)
 		sendDataEGG(7)
-	elif val <= 967 and val >= 962:
+	elif val <= 911 and val >= 905:
                 print ("8 egg",val)
 
                 #if (check == 7):
@@ -158,7 +157,7 @@ while True:
                  #       check = 8
                   #      sendDataEGG(8)
 		sendDataEGG(8)
-	elif val <= 961 and val >= 956:
+	elif val <= 904 and val >= 892:
                 print ("9 egg",val)
 
                 #if (check == 8):
@@ -170,7 +169,7 @@ while True:
                  #       check = 9
                   #      sendDataEGG(9)
 		sendDataEGG(9)
-	elif val <= 955 and val >= 900:
+	elif val <= 891 and val >= 700:
                 print ("10 egg",val)
 
                 #if (check == 9):
