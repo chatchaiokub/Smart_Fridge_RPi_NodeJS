b28594705a0859d5de754a9ae737131fa655471d
Chatchaiokub <chatchaiokub@Chatchaiokubs-MacBook-Pro-3.local>
2017-06-03 10:28:42 +0700

    line notify

---
+++ Alertdoor.py
@@ -0,0 +1,13 @@
+import requests,json
+import urllib.parse
+
+LINE_ACCESS_TOKEN="9qdZizFM5G6T767RFZLH1hdyyXnUCpDZw8WMzqyyrB1"
+url = "https://notify-api.line.me/api/notify"
+
+
+message ="The door isn't closed." # ข้อความที่ต้องการส่ง
+msg = urllib.parse.urlencode({"message":message})
+LINE_HEADERS = {'Content-Type':'application/x-www-form-urlencoded',"Authorization":"Bearer "+LINE_ACCESS_TOKEN}
+session = requests.Session()
+a=session.post(url, headers=LINE_HEADERS, data=msg)
+print(a.text)
