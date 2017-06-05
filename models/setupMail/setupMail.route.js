b0afbaf84b73e090b0d90876ab76f2acba7ef431
Chatchaiokub <chatchaiokub@Chatchaiokubs-MacBook-Pro-3.local>
2017-06-05 13:58:32 +0700

    mail

--- models/setupMail/setupMail.route.js
+++ models/setupMail/setupMail.route.js
@@ -3,33 +3,13 @@
   var express = require('express')
   var router = express.Router()
 
-  var SUBJECT = 'Ordering Product.'
+  var SUBJECT = 'Order Buy Drink.'
   var BODYTEXT = ''
 
   router.post('/setupMail', function (req, res) {
     BODYTEXT = req.body.bodyText
     res.send('success')
   })
-
-  // router.get('/setupEgg', function (req, res) {
-  //   var api_key = 'key-087ab4313404c2df9dab775cb3d0ecb9'
-  //   var domain = 'sandboxbbf5cfe12b8741589ab71636de046655.mailgun.org'
-  //   var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain})
-  //   var data = {
-  //     from: 'Smart-FridgeC <postmaster@sandboxbbf5cfe12b8741589ab71636de046655.mailgun.org>',
-  //     to: 'chatty30433@windowslive.com',
-  //     subject: 'Order Buy Eggs',
-  //     text: 'Buy Eggs 1 Dozen'
-  //   }
-  //   mailgun.messages().send(data, function (error, body) {
-  //     console.log(body)
-  //     if (!error) {
-  //       res.send('Mail Send')
-  //     }else {
-  //       res.send('Mail not Send')
-  //     }
-  //   })
-  // })
   router.get('/setupEgg', function (req, res) {
     var nodemailer = require('nodemailer')
     var smtpTransport = require('nodemailer-smtp-transport')
@@ -43,8 +23,8 @@
     var mailOptions = {
       from: '5606021612065@fitm.kmutnb.ac.th',
       to: 'chatty30433@windowslive.com',
-      subject: 'Sending Email using Node.js',
-      text: 'That was easy!'
+      subject: 'Order Buy Eggs',
+      text: 'Buy Eggs 1 Dozen'
     }
     transport.sendMail(mailOptions, function (error, info) {
       if (error) {
@@ -57,21 +37,28 @@
     })
   })
   router.get('/setupDrink', function (req, res) {
-    var api_key = 'key-b86bc2d406d41485a38a6290e26adde9'
-    var domain = 'sandbox340c66c365524888a8427b6a32210046.mailgun.org'
-    var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain})
-    var data = {
-      from: 'Smart-Fridge <postmaster@sandbox340c66c365524888a8427b6a32210046.mailgun.org>',
+    var nodemailer = require('nodemailer')
+    var smtpTransport = require('nodemailer-smtp-transport')
+    var transport = nodemailer.createTransport(smtpTransport({
+      service: 'gmail',
+      auth: {
+        user: '5606021612065@fitm.kmutnb.ac.th', // my mail
+        pass: 'anachakfitmokub@31'
+      }
+    }))
+    var mailOptions = {
+      from: '5606021612065@fitm.kmutnb.ac.th',
       to: 'chatty30433@windowslive.com',
       subject: SUBJECT,
       text: BODYTEXT
     }
-    mailgun.messages().send(data, function (error, body) {
-      console.log(body)
-      if (!error) {
+    transport.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error)
+        res.send('Mail Not Send')
+      } else {
+        console.log('Email sent: ' + info.response)
         res.send('Mail Send')
-      }else {
-        res.send('Mail not Send')
       }
     })
   })
