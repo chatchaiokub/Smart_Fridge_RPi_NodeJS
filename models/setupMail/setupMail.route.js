9bed2a5f5376a46ce93abbe121af36f843438cf6
Chatchaiokub <chatchaiokub@Chatchaiokubs-MacBook-Pro-3.local>
2017-06-05 10:37:40 +0700

    mail

--- models/setupMail/setupMail.route.js
+++ models/setupMail/setupMail.route.js
@@ -12,11 +12,11 @@
   })
 
   router.get('/setupEgg', function (req, res) {
-    var api_key = 'key-b86bc2d406d41485a38a6290e26adde9'
-    var domain = 'sandbox340c66c365524888a8427b6a32210046.mailgun.org'
+    var api_key = 'key-087ab4313404c2df9dab775cb3d0ecb9'
+    var domain = 'sandboxbbf5cfe12b8741589ab71636de046655.mailgun.org'
     var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain})
     var data = {
-      from: 'Smart-Fridge <postmaster@sandbox340c66c365524888a8427b6a32210046.mailgun.org>',
+      from: 'Smart-FridgeC <postmaster@sandboxbbf5cfe12b8741589ab71636de046655.mailgun.org>',
       to: 'chatty30433@windowslive.com',
       subject: 'Order Buy Eggs',
       text: 'Buy Eggs 1 Dozen'
