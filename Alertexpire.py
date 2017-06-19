85a6585a76ffb4c8e96fb41f60ac0df2bd54c433
Chatchaiokub <chatchaiokub@Chatchaiokubs-MacBook-Pro-3.local>
2017-06-20 06:10:24 +0700

    Line

--- Alertexpire.py
+++ Alertexpire.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-f
 import requests,json
 from urlparse import urlparse
 import urllib
@@ -5,7 +6,7 @@ import urllib
 LINE_ACCESS_TOKEN="9qdZizFM5G6T767RFZLH1hdyyXnUCpDZw8WMzqyyrB1"
 url = "https://notify-api.line.me/api/notify"
 
-message ="Food or drink expired."
+message ="อาหาร หรือ เครื่องดื่ม หมดอายุ !"
 msg = urllib.urlencode(({"message":message}))
 LINE_HEADERS = {'Content-Type':'application/x-www-form-urlencoded',"Authorization":"Bearer "+LINE_ACCESS_TOKEN}
 session = requests.Session()
